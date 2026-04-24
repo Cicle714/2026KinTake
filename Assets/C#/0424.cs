246690070b13d9cf3d05eb1c313760a9713d5e37
student <student>
2026-04-24 12:18:56 +0900

    Unity

---
+++ Assets/C#/0424.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class 0424 : MonoBehaviour
+{
+
+    int[] intval[];
+GameObject[] gameObjects;
+    
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    intval = new int[10];
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
